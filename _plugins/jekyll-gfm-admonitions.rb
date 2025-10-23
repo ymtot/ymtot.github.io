7dc9977c72f2780076b6dcd55223697e9ab7cc9f
ymtot <cutyting@outlook.com>
2025-10-23 15:34:26 +0800

    local test for admonitions

--- _plugins/jekyll-gfm-admonitions.rb
+++ _plugins/jekyll-gfm-admonitions.rb
@@ -126,7 +126,7 @@ module JekyllGFMAdmonitions
 
     GFMAdmonitionConverter.admonition_pages.each do |page|
       Jekyll.logger.debug 'GFMA:', "Appending admonition style to '#{page.path}'."
-      css = File.read(File.expand_path('../assets/admonitions.css', __dir__))
+      css = File.read(File.expand_path('./admonitions.css', __dir__))
 
       page.output.gsub!(%r{<head>(.*?)</head>}m) do |match|
         head = Regexp.last_match(1)
